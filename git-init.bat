261034650ff7de7dd3db946f74f77f1c69b8a22a
Daniel Pimenta <daniel.s.pimenta@gmail.com>
2019-08-09 16:19:56 -0300

    Estrutura RocketSeat

---
+++ git-init.bat
@@ -0,0 +1,6 @@
+echo "# Dotcom-React-Native-Nubank" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Daniel-Pimenta/Dotcom-React-Native-Nubank.git
+git push -u origin master
